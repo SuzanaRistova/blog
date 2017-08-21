0efa77a8e8cc81409616e9710baabe34ed7c2f56
suzana <suzanaristova@yahoo.com>
2017-08-21 17:41:06 +0200

    Display all users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Instantiate a new UserController instance.
+     */
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function index(){
+        $users = User::get();
+        return view('user.index', compact('users'));
+    }
+
+
+}
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,5 @@
+<h2>Users</h2>
+
+@foreach($users as $user)
+    <h4>{{ $user->name }}</h4>
+@endforeach
