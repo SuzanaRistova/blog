34994779afda1ff7a925fafccc7806fac215b560
suzana <suzanaristova@yahoo.com>
2017-08-25 16:23:52 +0200

    video

--- resources/views/session/view.blade.php
+++ resources/views/session/view.blade.php
@@ -9,8 +9,14 @@
                     <div class="title"><?= $session->title; ?></div>
                 </div>
                 <div class="body-container">
-                    <div class="content"><?= $session->content; ?></div>
+                    <div class="body-container">
+                        <label>Completed:</label>
                         <input id="completed" name="completed" type="checkbox" <?= ($session->completed == 0) ? "" : "checked" ?> value="{{ old('completed', $session->completed) }}">
+                    </div>
+                    <div class="content"><?= $session->content; ?></div>
+                    <div class="body-container">
+                        <iframe id="video" width="420" height="315" src="<?= $session->video ?>" frameborder="0" allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
         </div>
